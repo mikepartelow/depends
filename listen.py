fdda884714eb45f92ffd7c36bec84a8e0d05f0c7
Mike Partelow <github@mikepartelow.com>
2015-10-22 13:31:27 -0700

    works

--- listen.py
+++ listen.py
@@ -1,11 +1,37 @@
 from scapy.all import *
+import os
+
+
+def toggle_switch(switch_name):
+    status = os.popen("venv.depends/bin/wemo status").read()
+    switch_status = None
+    for line in status.splitlines():
+        if switch_name in line:
+            if '1' in line:
+                switch_status = 'on'
+            elif '0' in line:
+                switch_status = 'off'
+        else:
+            print("nope: {}".format(line.strip()))
+
+    if switch_status == 'on':
+        os.popen("venv.depends/bin/wemo switch '{}' off".format(switch_name)).read()
+    elif switch_status == 'off':
+        os.popen("venv.depends/bin/wemo switch '{}' on".format(switch_name)).read()
+    else:
+        print("found no status for {}".format(switch_name)) 
 
 def arp_display(pkt):
+  print("arp")
   if pkt[ARP].op == 1: #who-has (request)
+    print("arp1")
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      print("arp2")
       if pkt[ARP].hwsrc == '74:75:48:37:8e:c9':
-        print "Pushed Depend"
+          print("depend")
+          toggle_switch('Living Room Torch')
       else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
-print sniff(prn=arp_display, filter="arp", store=0, count=100)
+
+sniff(prn=arp_display, filter="arp")
