c03004c4fbcae19bdd217a71390fe844cfa6c36e
Mike Partelow <github@mikepartelow.com>
2015-10-25 09:21:56 -0700

    so clean so pristine so bucholic

--- listen.py
+++ listen.py
@@ -1,5 +1,6 @@
 from scapy.all import *
-import os
+from ouimeaux.environment import Environment
+import os, sys, json
 
 # TODO:
 # - read config file
@@ -9,56 +10,39 @@ import os
 # - either remove path dependencies or make an installer that alters them
 # - better name for this file
 # - consider making a Raspbian package (.deb?) for apt-get
+# - discovery tool for creating the conf
 
-WEMO='wemo'
-
-class WemoSwitch(object):
-    ON  = 'on'
-    OFF = 'off'
-
-    def __init__(self, name):
-        self.name = name
-
-    def status(self):
-        status_all = os.popen("{} status".format(WEMO)).read()
-        for line in status_all.splitlines():
-            if self.name in line:
-                if '1' in line:
-                    return self.ON
-                elif '0' in line:
-                    return self.OFF
-
-    def turn_on(self):
-        os.popen("{} switch '{}' on".format(WEMO, self.name)).read()
-
-    def turn_off(self):
-        os.popen("{} switch '{}' off".format(WEMO, self.name)).read()
-
-    def toggle(self):
-        my_status = self.status()
-        if my_status is self.ON:
-            self.turn_on()
-        elif my_status is self.OFF:
-            self.turn_off()
-    
-
-BUTTONS_FOR_WEMO_SWITCHES = {
-    '74:75:48:37:8e:c9' : WemoSwitch('Living Room Torch'),
-}
+button_switches = {}
 
 def arp_display(pkt):
-  if ARP in pkt and pkt[ARP].op == 1: #who-has (request)
-    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-      mac = pkt[ARP].hwsrc
-      if mac in BUTTONS_FOR_WEMO_SWITCHES:
-          wemo = BUTTONS_FOR_WEMO_SWITCHES[mac]
-          wemo.toggle()
+    if ARP in pkt and pkt[ARP].op == 1: #who-has (request)
+        if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+            mac = pkt[ARP].hwsrc
+            if mac in button_switches:
+	        wemo = button_switches[mac]
+		wemo.toggle()
 
+if len(sys.argv) < 2:
+    print("Usage: {} <path to config file>".format(sys.argv[0]))
+    sys.exit(1)
 
-mac = '74:75:48:37:8e:c9'
-if mac in BUTTONS_FOR_WEMO_SWITCHES:
-    wemo = BUTTONS_FOR_WEMO_SWITCHES[mac]
-    wemo.toggle()
+with open(sys.argv[1]) as conf:
+    config = json.loads(conf.read())
 
-# sniff(prn=arp_display, filter="arp", store=0)
+env = Environment()
+env.start()
+env.discover()
+env.wait(3)
+
+for switch_name in env.list_switches():
+    for button_switch in config:
+        if switch_name == button_switch['wemo name']:
+	    # FIXME: log
+	    print("Found Wemo: {}".format(switch_name))
+	    button_switches[button_switch['button mac']] = env.get(switch_name)
+
+# FIXME: log
+print("Sniffing for: {}".format(", ".join(button_switches.keys())))
+
+sniff(prn=arp_display, filter="arp", store=0)
 
